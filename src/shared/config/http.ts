bf2f76ff3096334c9af09f2ecaa16aa5c6d49f49
Bohdan Starchuk <bohdan.starchuk@gmail.com>
2024-07-22 09:17:17 +0300

    test

--- src/shared/config/http.ts
+++ src/shared/config/http.ts
@@ -1,7 +1,7 @@
 import globalAxios from 'axios';
 import { AuthRes } from './types';
 
-export const API_URL: string = import.meta.env.VITE_SERVER_URL;
+export const API_URL = import.meta.env.VITE_API_URL;
 
 export const http = globalAxios.create({
   baseURL: API_URL,
@@ -18,29 +18,41 @@ http.interceptors.response.use(
     return config;
   },
   async (error) => {
-    const originalRequest = error.config;
+    const accessToken = localStorage.getItem('access_token');
     const refreshToken = localStorage.getItem('refresh_token');
-    if (error.response.status === 401 && error.config && !error.config._isRetry && refreshToken) {
-      originalRequest._isRetry = true;
-      try {
+    const originalRequest = error.config;
+
+    try {
+      if (
+        error.response.status === 401 &&
+        error.config &&
+        !error.config._isRetry &&
+        accessToken &&
+        refreshToken
+      ) {
+        originalRequest._isRetry = true;
+
         const response = await globalAxios.post<AuthRes>(
           `${API_URL}/authentication/refresh-tokens`,
+          { refreshToken: refreshToken },
           {
             withCredentials: true,
-          },
-          {
             headers: {
               Authorization: `Bearer ${refreshToken}`,
             },
           },
         );
+
         localStorage.setItem('access_token', response.data.tokens.accessToken);
         localStorage.setItem('refresh_token', response.data.tokens.refreshToken);
+
         return http.request(originalRequest);
-      } catch (e) {
-        console.log('Not authorized');
       }
+    } catch (error) {
+      localStorage.removeItem('access_token');
+      localStorage.removeItem('refresh_token');
     }
-    throw error;
+
+    throw error.response.data.message;
   },
 );
